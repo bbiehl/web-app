0f6d3905103aee112906ac9fe613c9004b7f4a96
Bradley Biehl <bradley.biehl@gmail.com>
2022-08-17 19:48:50 -0500

    [develop] formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
     declarations: [AppComponent],
