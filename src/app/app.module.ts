3555df565b55550e789408d4e0b9eb58d4443019
Bradley Biehl <bradley.biehl@gmail.com>
2022-08-17 19:48:25 -0500

    [develop] added NgRx Effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        EffectsModule.forRoot([]),
         // Must import StoreModule before StoreDevtoolsModule. Otherwise Redux DevTools won't find store.
         StoreModule.forRoot({}, {}),
         StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
