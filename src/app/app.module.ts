422845f47aea1f62ac0d907702d0f15ce8e2d9ce
Bradley Biehl <bradley.biehl@gmail.com>
2022-08-17 19:35:07 -0500

    [develop] installed material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
         // Otherwise Redux DevTools won't find store.
         StoreModule.forRoot({}, {}),
         StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
