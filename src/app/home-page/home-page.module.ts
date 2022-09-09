60e8ea54048c13e0e708503a7f09997d5617890f
Bradley Biehl <bradley.biehl@gmail.com>
2022-09-08 20:26:20 -0500

    [episode] added primary theming

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { EpisodeOverviewModule } from '../episode/episode-overview/episode-overview.module';
+import { MaterialModule } from '../shared/material/material.module';
 import { HomePageComponent } from './home-page.component';
 
 @NgModule({
     declarations: [HomePageComponent],
     exports: [HomePageComponent],
-    imports: [CommonModule, EpisodeOverviewModule],
+    imports: [CommonModule, EpisodeOverviewModule, FlexLayoutModule, MaterialModule],
 })
 export class HomePageModule {}
