52bc2f96164b46a52a2e99b92df22f95e6acd7d5
Bradley Biehl <bradley.biehl@gmail.com>
2022-09-27 20:50:54 -0500

    [episode] set up redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { EpisodeRoutingModule } from './episode/episode-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [{ path: '', component: HomePageComponent, title: 'Welcome to Analog Jones' }];
+const routes: Routes = [
+    { path: '', component: HomePageComponent, title: 'Welcome to Analog Jones' },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
     exports: [RouterModule],
--- src/app/episode/episode-routing.module.ts
+++ src/app/episode/episode-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { Categories } from 'src/app/shared/types/categories.enum';
 import { EpisodePageContainerComponent } from './container-components/episode-page-container/episode-page-container.component';
 
-const routes: Routes = [{ path: Categories.EPISODES, component: EpisodePageContainerComponent, title: 'All Episodes' }];
+const routes: Routes = [
+    { path: Categories.EPISODES, component: EpisodePageContainerComponent, title: 'All Episodes' },
+    { path: '**', redirectTo: Categories.EPISODES, pathMatch: 'full' },
+];
 
 @NgModule({
     exports: [RouterModule],
