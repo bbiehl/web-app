c7e4608d2dce48947725411a746ac5399db4c89d
Bradley Biehl <bradley.biehl@gmail.com>
2022-10-04 22:07:11 -0500

    [episode] set logic to handle redirects

--- src/app/episode/episode-detail/episode-detail.component.ts
+++ src/app/episode/episode-detail/episode-detail.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { Categories } from 'src/app/shared/types/categories.enum';
 import { selectEpisode } from '../store/episode.selector';
 import { Episode } from '../types/episode.model';
 
+@UntilDestroy()
 @Component({
     selector: 'app-episode-detail',
     templateUrl: './episode-detail.component.html',
@@ -12,9 +16,13 @@ import { Episode } from '../types/episode.model';
 export class EpisodeDetailComponent implements OnInit {
     episode$: Observable<Episode | undefined> = this.store.select(selectEpisode);
 
-    constructor(private store: Store) {}
+    constructor(private router: Router, private store: Store) {}
 
     ngOnInit(): void {
-        this.episode$.subscribe((ep) => console.log(ep));
+        this.episode$.pipe(untilDestroyed(this)).subscribe((ep) => {
+            if (ep === undefined) {
+                this.router.navigateByUrl(Categories.EPISODES);
+            }
+        });
     }
 }
