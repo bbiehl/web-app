33e05459ce2d535e0f65ace08e71bbfb6b89771d
Bradley Biehl <bradley.biehl@gmail.com>
2022-11-22 18:55:06 -0600

    [episode-detail] wip

--- src/app/episode/store/episode.selector.ts
+++ src/app/episode/store/episode.selector.ts
@@ -3,9 +3,7 @@ import { selectRouteParams } from 'src/app/router.selectors';
 import { episodeAdapter, EpisodeFeatureKey, EpisodeState } from './episode.reducer';
 
 export const selectEpisodeState = createFeatureSelector<EpisodeState>(EpisodeFeatureKey);
-
 export const { selectEntities, selectAll } = episodeAdapter.getSelectors();
-
 export const selectAllEpisodes = createSelector(selectEpisodeState, selectAll);
 export const selectEpisodeEntities = createSelector(selectEpisodeState, selectEntities);
 export const selectEpisode = createSelector(
