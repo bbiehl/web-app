ecd45c953dd2284390261a66ebc46b213654ac5b
Bradley Biehl <bradley.biehl@gmail.com>
2022-11-26 12:35:14 -0600

    [flex-layout] fixed nav

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../shared/material/material.module';
 import { NavigationComponent } from './navigation.component';
@@ -8,6 +7,6 @@ import { NavigationComponent } from './navigation.component';
 @NgModule({
     declarations: [NavigationComponent],
     exports: [NavigationComponent],
-    imports: [CommonModule, FlexLayoutModule, MaterialModule, RouterModule],
+    imports: [CommonModule, MaterialModule, RouterModule],
 })
 export class NavigationModule {}
