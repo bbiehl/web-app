e91e0f9ad0e5b1283b2114ec580200fa27264943
Bradley Biehl <bradley.biehl@gmail.com>
2022-11-26 16:20:05 -0600

    [flex-layout] fixed episode-summary

--- src/app/episode/episode-summary/episode-summary.module.ts
+++ src/app/episode/episode-summary/episode-summary.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { EpisodeSummaryComponent } from './episode-summary.component';
@@ -8,6 +7,6 @@ import { EpisodeSummaryComponent } from './episode-summary.component';
 @NgModule({
     declarations: [EpisodeSummaryComponent],
     exports: [EpisodeSummaryComponent],
-    imports: [CommonModule, FlexLayoutModule, MaterialModule, RouterModule],
+    imports: [CommonModule, MaterialModule, RouterModule],
 })
 export class EpisodeSummaryModule {}
